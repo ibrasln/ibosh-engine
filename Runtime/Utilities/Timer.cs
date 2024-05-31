60b624a7435c209c553df9a8ec2e72e40851e55c
Ibrahim Aslan <i.aslan2020@gtu.edu.tr>
2024-05-31 16:42:12 +0300

    [Utilities] Add Timer

---
+++ Runtime/Utilities/Timer.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+namespace IboshEngine.Runtime.Utilities
+{
+    public class Timer
+    {
+        public event Action OnTimerDone;
+        
+        private float _startTime;
+        private readonly float _duration;
+        private float _targetTime;
+
+        private bool _isActive;
+
+        public Timer(float duration)
+        {
+            _duration = duration;
+        }
+
+        public void StartTimer()
+        {
+            _startTime = Time.time;
+            _targetTime = _startTime + _duration;
+            _isActive = true;
+        }
+
+        public void StopTimer() => _isActive = false;
+
+        public void Tick()
+        {
+            if (!_isActive) return;
+
+            if (!(Time.time >= _targetTime)) return;
+            
+            OnTimerDone?.Invoke();
+            StopTimer();
+        }
+    }
+}
