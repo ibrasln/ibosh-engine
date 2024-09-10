8f283993b076cf97db87a05b7b41fddb48174397
Ibrahim Aslan <75907428+ibrasln@users.noreply.github.com>
2024-09-10 22:49:11 +0300

    Rename assembly definitions

--- Runtime/Utilities/Lifetime.cs
+++ Runtime/Utilities/Lifetime.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 namespace IboshEngine.Runtime.Utilities
 {
+    /// <summary>
+    /// A utility class that automatically destroys a GameObject after a specified lifetime.
+    /// </summary>
+    /// <remarks>
+    /// This class allows you to set a duration in seconds, after which the GameObject is destroyed.
+    /// It uses Unity's Destroy method to remove the object from the scene.
+    /// </remarks>
     public class Lifetime : MonoBehaviour
     {
         [SerializeField] private float lifetime;
